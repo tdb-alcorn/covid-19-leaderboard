026f2f6bf912c8ca6b858072fcd4965bbe94ea34
Tom Alcorn <tdb.alcorn@gmail.com>
2020-03-20 16:43:39 -0700

    Init heroku python

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("hi");
-}).listen(process.env.PORT);
